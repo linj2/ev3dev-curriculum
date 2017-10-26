8b17687aa847c84128910708fa9dab708c519afd
ChengqianLyu <lyuc@rose-hulman.edu>
2017-10-26 13:09:46 -0400

    m1 DONE

--- sandbox/lyuc/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/lyuc/digital_inputs/m1_button_states_with_leds.py
@@ -8,7 +8,7 @@ Much like we did in linj2 of the motors unit, later we will show you different (
 Authors: David Fisher and Chengqian Lyu.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
 #   You will exit the program by pressing the back button on the EV3 brick (button just below the screen).
 #   The back button is already implemented to exit the program (as you can see in the code below).
 
@@ -35,7 +35,7 @@ def main():
     ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
     time.sleep(3)
 
-    # Turn LEDs off
+    # Turn LEDs of
     ev3.Sound.speak("Off")
     ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
     ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
@@ -65,13 +65,12 @@ def main():
             print("right")
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
-        if btn.down:
-            while True:
-                current_color_index = current_color_index + 1
-                ev3.Leds.set_color(ev3.Leds.LEFT,led_colors[current_color_index])
-                ev3.Leds.set_color(ev3.Leds.RIGHT, led_colors[current_color_index])
-                if current_color_index > 3:
-                    current_color_index = 0
+
+        while btn.down:
+            current_color_index = (current_color_index + 1) % len(led_colors)
+            ev3.Leds.set_color(ev3.Leds.LEFT,led_colors[current_color_index])
+            ev3.Leds.set_color(ev3.Leds.RIGHT, led_colors[current_color_index])
+            time.sleep(2)
 
         # DONE: 3. Implement the left, right, and up buttons as follows:
         #    When the up button is being pressed:
@@ -91,7 +90,7 @@ def main():
         #     there just to provide you with code examples for using the LEDs.  It does not need to run anymore.
         #     Just make sure not to comment out too much. ;)
 
-        # TODO: 4. Implement the down button to change the color of both LEDs.
+        # DONE: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.
         #   If the user presses the down button again, wrap around the list to GREEN and continue as before.
         #   If the user holds down the button, figure out how to make the color change still only happen once.
@@ -99,7 +98,7 @@ def main():
         #     with a while loop that blocks code execution until the down instance variable is False.
         #     Use a time.sleep(0.01) inside the while loop to do nothing but wait for the button to be released.
 
-        # TODO: 5. Formally test your work. When you think you have the problem complete run these tests:
+        # DONE: 5. Formally test your work. When you think you have the problem complete run these tests:
         #   Press Left - Green left LED is on (try holding the button down for a few seconds when you to the press)
         #   Press Right - Right right LED is on
         #   Press Up - Both LEDs are off
@@ -111,7 +110,7 @@ def main():
         #   Press Down - Both LEDs are Red (the cycle repeats)
         #   Press Back - Both LEDs turn Green, the robot says Goodbye and the program exits
 
-        # TODO: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+        # DONE: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
         #
         # Observation you should make, working with buttons as 'states' is functional but usually 'events' work better.
         # Also observe that we don't use the Enter button.  Enter can cause issues since your program is running at the
