8c45f14fd1de5ec60b32edd69f0001b8d5bd3804
Jing Lin <linj2@rose-hulman.edu>
2017-10-31 20:00:12 -0400

    m1

--- sandbox/linj2/mqtt/m1_pc_shared_circles.py
+++ sandbox/linj2/mqtt/m1_pc_shared_circles.py
@@ -79,8 +79,8 @@ def main():
 
     my_delegate = MyDelegate(canvas)
     mqtt_client = com.MqttClient(my_delegate)
-    # mqtt_client.connect("draw", "draw")
-    mqtt_client.connect("draw","draw","broker.mqttdashboard.com")
+    mqtt_client.connect("draw", "draw")
+    # mqtt_client.connect("draw","draw","broker.mqttdashboard.com")
 
     root.mainloop()
 
@@ -99,10 +99,10 @@ def left_mouse_click(event, mqtt_client):
 
     # Optional test: If you just want to see circles purely local to your computer the four lines below would work.
     # You could uncomment it to see it temporarily, but make sure to comment it back out before todo7 below.
-    # canvas = event.widget
-    # canvas.create_oval(event.x - 10, event.y - 10,
-    #                    event.x + 10, event.y + 10,
-    #                    fill=my_color, width=3)
+    canvas = event.widget
+    canvas.create_oval(event.x - 10, event.y - 10,
+                       event.x + 10, event.y + 10,
+                       fill=my_color, width=3)
     # Repeated: If you uncommented the code above to test it, make sure to comment it back out before todo7 below.
 
     # MQTT draw
@@ -113,7 +113,7 @@ def left_mouse_click(event, mqtt_client):
     # Review the lecture notes about the two parameters passed into the mqtt_client.send_message method if needed
     # All of your teammates should receive the message and create a circle of your color at your click location.
     # Additionally you will receive your own message and draw a circle in your color too.
-
+    mqtt_client.send_message('on_circle_draw',[my_color, event.x, event.y])
     # TODO: 8. Help get everyone on your team running this program at the same time.
     # You should be able to see circles on your computer from everyone else on your team.
     # Try to draw the first letter of your name in circles. :)
