3a3243bcd47c8450b811c63cc434c915e99284a3
ChengqianLyu <lyuc@rose-hulman.edu>
2017-11-07 21:31:46 -0500

    done

--- sandbox/linj2/analog_sensors/pixy/m2_ev3_pixy_display.py
+++ sandbox/linj2/analog_sensors/pixy/m2_ev3_pixy_display.py
@@ -40,7 +40,7 @@ def main():
         print("(X, Y) = ({},{}))   Width = {} Height = {}".format(x, y, width, height))
         # done: 4. Send the Pixy values to the PC by calling the on_rectangle_update method
         # If you open m2_pc_pixy_display you can see the parameters for that method [x, y, width, height]
-        mqtt_client.send_message("on_rectangle_update", [x, y, width, height])
+        mqtt_client.send_messcage("on_rectangle_update", [x, y, width, height])
 
         time.sleep(0.25)
 
